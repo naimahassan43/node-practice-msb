3c69df22c2b85a6cd237b66f1ab1d9b6f64a02d8
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 11:40:04 +0000

    Mini Calculator:import&export modules

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+//Problem: Mini Calculator
+//Take Input from command line
 
+const add = require("./calculator/add");
+const sub = require("./calculator/sub");
+const mul = require("./calculator/mul");
+const div = require("./calculator/div");
