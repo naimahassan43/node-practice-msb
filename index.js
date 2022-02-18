18b7fdae4b247ce1e91f03acd9a98fcdbf8b2586
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 11:50:56 +0000

    File Sys Mod: Create file

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-//Problem: Mini Calculator
-//Take Input from command line
+//File System Module(with callback)
 
-const add = require("./calculator/add");
-const sub = require("./calculator/sub");
-const mul = require("./calculator/mul");
-const div = require("./calculator/div");
+const fs = require("fs");
 
-//Take Input from command line
-
-const a = parseInt(process.argv[2]);
-const ch = process.argv[3];
-const b = parseInt(process.argv[4]);
-
-if (ch === "ADD") {
-  console.log(add(a, b));
-} else if (ch === "SUB") {
-  console.log(sub(a, b));
-} else if (ch === "MUL") {
-  console.log(mul(a, b));
-} else if (ch === "DIV") {
-  console.log(div(a, b));
-} else {
-  console.log("Invalid");
-}
+//Create a file
+fs.writeFile("./app.txt", "Hello World", (err) => {
+  if (err) console.log(err);
+  else console.log("File created successfully");
+});
