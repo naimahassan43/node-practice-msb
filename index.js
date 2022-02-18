a5386b8e64b79498a9a8803d0d8f2062a8bab83c
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 11:57:32 +0000

    File Sys Mod(Promises): Read file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const fs = require("fs").promises;
 
 async function fileSystemOptions() {
   //Create a file
-  await fs.writeFile("./app.txt", "Hello World!");
+  // await fs.writeFile("./app.txt", "Hello World!");
+
+  //Read a file
+  const data = await fs.readFile("./app.txt", "utf-8");
+  console.log(data);
 }
 fileSystemOptions();
