1c0b3d1802261f26ae237ccbfe036038190f252b
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 11:47:47 +0000

    Mini Calculator:Take input from command line

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ const add = require("./calculator/add");
 const sub = require("./calculator/sub");
 const mul = require("./calculator/mul");
 const div = require("./calculator/div");
+
+//Take Input from command line
+
+const a = parseInt(process.argv[2]);
+const ch = process.argv[3];
+const b = parseInt(process.argv[4]);
+
+if (ch === "ADD") {
+  console.log(add(a, b));
+} else if (ch === "SUB") {
+  console.log(sub(a, b));
+} else if (ch === "MUL") {
+  console.log(mul(a, b));
+} else if (ch === "DIV") {
+  console.log(div(a, b));
+} else {
+  console.log("Invalid");
+}
