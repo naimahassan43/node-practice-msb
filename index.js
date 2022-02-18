0078fa61da9bb10e93b76c887b441a5ad7b34d6b
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 11:51:48 +0000

    File Sys Mod: Read file

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 const fs = require("fs");
 
 //Create a file
-fs.writeFile("./app.txt", "Hello World", (err) => {
+// fs.writeFile("./app.txt", "Hello World", (err) => {
+//   if (err) console.log(err);
+//   else console.log("File created successfully");
+// });
+
+//Read a file
+fs.readFile("./app.txt", "utf-8", (err, data) => {
   if (err) console.log(err);
-  else console.log("File created successfully");
+  else console.log(data);
 });
